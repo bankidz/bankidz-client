74ad88074da57c2a5901b45f53883431a511d94f
한규진 <david0218@naver.com>
2022-07-01 00:01:44 +0900

    feat: docker ci/cd 4트

---
+++ config/scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Installing docker engine if not exists
+if ! type docker > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install -y docker-ce
+fi
+
+# Installing docker-compose if not exists
+if ! type docker-compose > /dev/null
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+
+# # aws log setting
+#   sudo systemctl daemon-reload
+#   sudo service docker restart
