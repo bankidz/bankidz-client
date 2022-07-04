f95f519beda42913efe7bb9428e6d543428ee371
24siefil <24siefil@gmail.com>
2022-07-04 16:31:13 +0900

    chore: 테스트 페이지 코드 정리

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -5,10 +5,6 @@ function TestPage() {
   return (
     <ComponentWrapper>
       <span>컴포넌트 랜더링 테스트용 페이지 입니다.</span>
-      <ProfileButton role={'아빠'} />
-      {/* <ProfileButton role={'아빠'}/> */}
-      {/* <ProfileButton role={'아빠'}/> */}
-      {/* <ProfileButton role={'아빠'}/> */}
     </ComponentWrapper>
   );
 }
