da36b5d575cae8482a12e4d9c206e138a299a698
24siefil <24siefil@gmail.com>
2022-07-05 21:16:40 +0900

    feat: ChallengeBadge

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,10 +1,26 @@
 import styled from 'styled-components';
+import InterestBadge from '../components/challenge/InterestBadge';
+import ChallengeBadge, {
+  TargetBadge,
+} from '../components/challenge/TargetBadge';
 
 function TestPage() {
   // 컴포넌트 랜더링 테스트용 페이지 입니다.
-  return <ComponentWrapper></ComponentWrapper>;
+  return (
+    <ComponentWrapper>
+      <TargetBadge targetSaving={100000} />
+      <InterestBadge interestRate={10} />
+    </ComponentWrapper>
+  );
 }
 
 export default TestPage;
 
-const ComponentWrapper = styled.div``;
+const ComponentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
