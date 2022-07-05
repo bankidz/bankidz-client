8e6f11e6c3365f4505bc7db90929eb2e8d96dc69
24siefil <24siefil@gmail.com>
2022-07-05 11:29:44 +0900

    feat: InterestBadge

---
+++ src/components/badges/InterestBadge.tsx
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components';
+
+interface InterestBadgeProps {
+  /** 이자율(number)을 입력합니다. */
+  interestRate: number;
+}
+
+function InterestBadge({ interestRate }: InterestBadgeProps) {
+  const label = '이자율 ' + interestRate.toString() + '%';
+  console.log(label);
+  return (
+    <ComponentWrapper>
+      <StyledSpan>{label}</StyledSpan>
+    </ComponentWrapper>
+  );
+}
+
+export default InterestBadge;
+
+const ComponentWrapper = styled.div`
+  height: 27px;
+`;
+
+const StyledSpan = styled.span`
+  width: 90px;
+  height: 27px;
+
+  background: ${({ theme }) => theme.palette.yellow[2]};
+  border-radius: 12px;
+
+  line-height: 27px;
+  vertical-align: center;
+  display: inline-block;
+  text-align: center;
+
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+`;
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 import SuggestBadge from '../components/badges/SuggestBadge';
 import ProfileButton from '../components/onboarding/ProfileButton';
 import { darken, lighten } from 'polished';
+import InterestBadge from '../components/badges/InterestBadge';
 
 function TestPage() {
   return (
     <ComponentWrapper>
-      <button>Sample</button>
-      <ProfileButton role="아빠" />
+      {/* <ProfileButton role="아빠" />
       <ProfileButton role="엄마" isSelected />
       <SuggestBadge isSuggesting={true} />
-      <SuggestBadge isSuggesting={false} />
+      <SuggestBadge isSuggesting={false} /> */}
+      <InterestBadge interestRate={10} />
       <span>컴포넌트 랜더링 테스트용 페이지 입니다.</span>
     </ComponentWrapper>
   );
