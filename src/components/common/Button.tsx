02f0a8aa6db6c8668d03abecb6ebc6907608ffff
한규진 <david0218@naver.com>
2022-07-08 02:31:17 +0900

    feat : 버튼 스타일링 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -16,16 +16,27 @@ interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
    * 버튼 활성화 상태
    */
   state?: boolean;
+  /**
+   * 강조되지 않은 버튼
+   */
+  sub?: boolean;
 }
 
 function Button({
   property = 'primary',
   label,
   state = true,
+  sub = false,
   ...props
 }: ButtonProps) {
   return (
-    <Wrapper property={property} state={state} disabled={!state} {...props}>
+    <Wrapper
+      property={property}
+      state={state}
+      disabled={!state}
+      sub={sub}
+      {...props}
+    >
       {property == 'etc' && <img src={kakao} />}
       <p>{label}</p>
     </Wrapper>
@@ -37,36 +48,43 @@ export default Button;
 const Wrapper = styled.button<{
   property: 'etc' | 'primary' | 'secondary';
   state: boolean;
+  sub: boolean;
 }>`
   display: flex;
   align-items: center;
   justify-content: center;
   width: ${({ property }) => (property == 'secondary' ? '160px' : '100%')};
   height: 50px;
-  border-radius: 8px;
+  border-radius: 12px;
   border: none;
   font-size: 16px;
   font-weight: bold;
   cursor: pointer;
 
   ${({ property }) =>
-    property == 'etc'
-      ? css`
-          color: #191919;
-          background-color: #fee500;
+    property == 'etc' &&
+    css`
+      color: #191919;
+      background-color: #fee500;
 
-          img {
-            display: flex;
-            padding-right: 280px;
-          }
-          p {
-            position: absolute;
-          }
+      img {
+        display: flex;
+        padding-right: 280px;
+      }
+      p {
+        position: absolute;
+      }
+    `};
+  ${({ sub }) =>
+    sub
+      ? css`
+          color: ${({ theme }) => theme.palette.gray[5]};
+          background-color: ${({ theme }) => theme.palette.gray[2]};
         `
       : css`
           color: ${({ theme }) => theme.palette.white};
           background-color: ${({ theme }) => theme.palette.yellow[0]};
-        `};
+        `}
 
   :disabled {
     background-color: ${({ theme }) => theme.palette.gray[2]};
