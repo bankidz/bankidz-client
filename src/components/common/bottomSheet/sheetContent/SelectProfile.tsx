11ae76e756028e448de5b69b95ea5c802319909d
한규진 <david0218@naver.com>
2022-07-07 16:45:29 +0900

    feat : 바텀시트 - 프로필 선택 확인 컴포넌트

---
+++ src/components/common/bottomSheet/sheetContent/SelectProfile.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import Button from '../../Button';
+import { ReactComponent as DadBanky } from '../../../../assets/banky/dad.svg';
+
+interface SelectProfileProps {
+  role: '엄마' | '아빠' | '아들' | '딸';
+  onClick: () => void;
+}
+
+function SelectProfile({ role, onClick }: SelectProfileProps) {
+  return (
+    <Wrapper>
+      <Container>
+        <p>
+          {role}
+          {role === '아들' || role === '딸' ? '이' : '가'} 맞나요?
+        </p>
+        <p>한 번 설정한 프로필은 변경하기 어려워요</p>
+        <BankyWrapper>
+          <DadBanky />
+        </BankyWrapper>
+      </Container>
+      <Button label="확인" />
+    </Wrapper>
+  );
+}
+
+export default SelectProfile;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div`
+  margin: 36px 16px;
+  margin-top: 29px;
+  display: grid;
+  grid-template-rows: auto;
+  grid-gap: 16px;
+
+  & > p:first-child {
+    font-family: 'TmoneyRoundWind';
+    font-size: 21px;
+    line-height: 21px;
+    font-weight: 800;
+    color: ${({ theme }) => theme.palette.black};
+  }
+  & > p:nth-child(2) {
+    font-family: 'TmoneyRoundWind';
+    font-size: 14px;
+    line-height: 14px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.palette.gray[5]};
+  }
+`;
+
+const BankyWrapper = styled.div`
+  height: 140px;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+`;
