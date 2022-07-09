31d83682884b5acb791d7667e7010cac3cac5a84
24siefil <24siefil@gmail.com>
2022-07-09 12:44:11 +0900

    refactor: 지금까지 제작한 모든 컴포넌트 반응형 고려, 코딩 컨벤션에 맞추어 리팩토링

---
+++ src/components/badges/InterestBadge.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+interface InterestBadgeProps {
+  /** 이자율(number)을 입력합니다. */
+  interestRate: number;
+}
+
+function InterestBadge({ interestRate }: InterestBadgeProps) {
+  const label = '이자율 ' + interestRate.toString() + '%';
+  return (
+    <Wrapper>
+      <StyledSpan>{label}</StyledSpan>
+    </Wrapper>
+  );
+}
+
+export default InterestBadge;
+
+const Wrapper = styled.div`
+  height: 27px;
+`;
+
+const StyledSpan = styled.span`
+  width: 90px;
+  height: 27px;
+
+  background: ${({ theme }) => theme.palette.yellow[2]};
+  border-radius: 12px;
+
+  line-height: 27px;
+  vertical-align: center;
+  display: inline-block;
+  text-align: center;
+
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+`;
--- src/pages/SungwooTestPage.tsx
+++ src/pages/SungwooTestPage.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import TargetBadge from '../components/challenge/TargetBadge';
 import Summary from '../components/homekid/Summary';
 import ProfileButton from '../components/onboarding/ProfileButton';
 
@@ -7,8 +6,6 @@ function SungwooTestPage() {
   return (
     <Wrapper>
       <ProfileButton role="아빠" isSelected={false} />
-      {/* <InterestBoosterBadge interestRate={10} /> */}
-      {/* <TargetBadge targetSaving={100000} /> */}
       <Summary current={1000} goal={5000} month={6} week={4} />
     </Wrapper>
   );
