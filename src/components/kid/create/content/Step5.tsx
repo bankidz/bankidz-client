1dc8185bd31fc087eeac84a314a02ed60b12d79a
한규진 <david0218@naver.com>
2022-07-19 02:18:55 +0900

    feat : step5 돈길 계약하기 사인하기 모듈

--- src/components/kid/create/content/Step5.tsx
+++ src/components/kid/create/content/Step5.tsx
@@ -1,5 +1,29 @@
+import ContractSheet from '@components/common/bottomSheet/ContractSheet';
+import Signature from '@components/common/bottomSheet/sheetContent/Signature';
+import { useAppDispatch } from '@store/app/hooks';
+import { useState } from 'react';
+
 function Step5({ currentStep }: { currentStep: number }) {
-  return <></>;
+  const dispatch = useAppDispatch();
+  const [disabledNext, setDisabledNext] = useState<boolean>(true);
+  const [sign, setSign] = useState();
+  // 다음으로 버튼 클릭
+  const onClickNextButton = () => {
+    console.log(sign);
+  };
+  return (
+    <>
+      <ContractSheet
+        open={true}
+        label={'다음'}
+        onDismiss={() => {}}
+        onClickNext={onClickNextButton}
+        disabledNext={disabledNext}
+      >
+        <Signature setDisabledNext={setDisabledNext} setSign={setSign} />
+      </ContractSheet>
+    </>
+  );
 }
 
 export default Step5;
