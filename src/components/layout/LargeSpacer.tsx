3f6ffe73c4a3e40bc16daeed5224dfb25474489b
신성우 <24siefil@gmail.com>
2022-08-04 12:21:33 +0900

    fix: 화면 크기 작은 기기에서 모달 잘림

--- src/components/layout/LargeSpacer.tsx
+++ src/components/layout/LargeSpacer.tsx
@@ -9,8 +9,10 @@ export default LargeSpacer;
 // TabBar height: 47px;
 // TabBar 있는 경우 LargeSpacer(height: 96), TabBar 없는 경우 SmallSpacer(height: 48) 사용
 const StyledDiv = styled.div<{ isWhite: boolean }>`
-  height: 96px;
+  /* height: 96px; */
+  height: 200px;
   width: 100%;
+  background: palegreen;
   background: ${({ theme, isWhite }) =>
     isWhite ? theme.palette.greyScale.white : theme.palette.greyScale.grey100};
 `;
