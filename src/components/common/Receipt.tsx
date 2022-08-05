952329185738d5cec9a74750fd32972d696a1633
신성우 <24siefil@gmail.com>
2022-08-05 14:03:51 +0900

    fix: 계약종료 주차 -> 계약종료일 워딩 수정

--- src/components/common/Receipt.tsx
+++ src/components/common/Receipt.tsx
@@ -90,7 +90,7 @@ ReceiptProps) {
           </div>
           <div className="계약종료주차">
             <div className="text-wrapper">
-              <div className="title">계약종료 주차</div>
+              <div className="title">계약종료일</div>
               <div className="content">
                 {`${year}년 ${month}월 ${weekNo}주`}
               </div>
