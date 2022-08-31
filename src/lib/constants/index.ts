a168dce4a2bdf90192fbdbb10eff846b58686d63
24siefil <24siefil@gmail.com>
2022-08-31 10:53:23 +0900

    feat: apple login query param 형태로 변경

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -3,8 +3,8 @@
  ** API Base URL
  ** =============================================================================
  */
-// export const BASE_URL = 'https://api.bankidz.com'; // 배포 환경
-export const BASE_URL = 'https://bankids.click'; // 테스트 환경
+export const BASE_URL = 'https://api.bankidz.com'; // 배포 환경
+// export const BASE_URL = 'https://bankids.click'; // 테스트 환경
 
 /*
  ** =============================================================================
