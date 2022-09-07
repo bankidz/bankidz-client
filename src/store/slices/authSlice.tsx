870efafea8a8766dcea540b31ac0249fd9c19676
한규진 <david0218@naver.com>
2022-09-07 22:15:08 +0900

    fix : 돈길 걷기 뱅키 크기

--- src/store/slices/authSlice.tsx
+++ src/store/slices/authSlice.tsx
@@ -27,8 +27,7 @@ interface IAuthState {
 
 const initialState: IAuthState = {
   auth: {
-    accessToken:
-      'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NjE0ODQyMTQsInN1YiI6IjUiLCJleHAiOjE2NjI2OTM4MTQsImlkIjo1LCJyb2xlcyI6IlVTRVIifQ.5fKVlH-BGRRXiSP2WFtiLGheiNThQAC8wc7yj38MAG8',
+    accessToken: '',
     isKid: false,
     level: null,
     provider: '',
