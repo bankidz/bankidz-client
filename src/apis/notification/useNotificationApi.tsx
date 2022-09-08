be9327ca9d5d370d53fabe833aa275c49f202bf3
한규진 <david0218@naver.com>
2022-09-08 18:12:22 +0900

    feat : 새 알림 여부

--- src/apis/notification/useNotificationApi.tsx
+++ src/apis/notification/useNotificationApi.tsx
@@ -3,6 +3,7 @@ import { INotificationDTO } from './notification.dto';
 const useNotificationApi = () => {
   const axiosPrivate = useAxiosPrivate();
 
+  // 알림 내역 가져오기 (무한스크롤)
   const getNotification = async ({
     pageParam = '',
   }): Promise<INotificationDTO> => {
@@ -13,13 +14,21 @@ const useNotificationApi = () => {
     return data;
   };
 
+  // 알림 읽은 처리
   const patchNotificationIsRead = async (id: number) => {
     const response = await axiosPrivate.patch(`/notification/${id}`);
     const data = response.data.data;
     return data;
   };
 
-  return { getNotification, patchNotificationIsRead };
+  // 새 알림 여부
+  const getNotificationIsAllRead = async (): Promise<boolean> => {
+    const response = await axiosPrivate.get(`/notification/isRead`);
+    const data = response.data.data;
+    return data.isAllRead;
+  };
+
+  return { getNotification, patchNotificationIsRead, getNotificationIsAllRead };
 };
 
 export default useNotificationApi;
--- src/lib/constants/QUERY_KEY.ts
+++ src/lib/constants/QUERY_KEY.ts
@@ -14,4 +14,8 @@ export const USER = 'user';
  */
 export const NOTICE = 'notice';
 
+/**
+ * notification api 관련
+ */
 export const NOTIFICATION = 'notification';
+export const NOTIFICATION_IS_READ = 'notificationIsRead';
