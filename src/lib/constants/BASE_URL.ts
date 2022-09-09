c30c2223cd66438a0248198fe9db7e0851c3f34a
24siefil <24siefil@gmail.com>
2022-09-10 00:09:10 +0900

    안녕히계세요 제발요

--- src/lib/constants/BASE_URL.ts
+++ src/lib/constants/BASE_URL.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.bankidz.com'; // 배포 환경
-//export const BASE_URL = 'https://bankids.click'; // 테스트 환경
+// export const BASE_URL = 'https://bankids.click'; // 테스트 환경
