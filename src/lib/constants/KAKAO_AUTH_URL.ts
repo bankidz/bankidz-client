23c92aabc63a1af03ea9efcc2bdf506e7b75f296
24siefil <24siefil@gmail.com>
2022-09-11 22:55:39 +0900

    refactor: 로컬, 배포환경 환경변수 다르게 적용

--- src/lib/constants/KAKAO_AUTH_URL.ts
+++ src/lib/constants/KAKAO_AUTH_URL.ts
@@ -1,5 +1,6 @@
 // const DOMAIN = 'https://bankidz.com'; // 배포 환경(고정)
-const DOMAIN = 'http://localhost:3000'; // 테스트 환경
+// const DOMAIN = 'http://localhost:3000'; // 테스트 환경
+const DOMAIN = `${process.env.REACT_APP_DOMAIN}`;
 const REST_API_KEY = `${process.env.REACT_APP_KAKAO_REST_API_KEY}`;
 const REDIRECT_URI = `${DOMAIN}/auth/kakao/callback`;
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
