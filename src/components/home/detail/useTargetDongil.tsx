532c2333aab471e243a47b4982887fdb0ecbf9cc
24siefil <24siefil@gmail.com>
2022-09-17 14:31:04 +0900

    refactor: 자녀 /DetailPage RQ migration

--- src/components/home/detail/useTargetDongil.tsx
+++ src/components/home/detail/useTargetDongil.tsx
@@ -1,14 +1,20 @@
+import challengeAPI from '@lib/apis/challenge/challengeAPI';
+import queryKeys from '@lib/constants/queryKeys';
 import { useAppSelector } from '@store/app/hooks';
 import { selectIsKid } from '@store/slices/authSlice';
 import { selectSelectedKid } from '@store/slices/kidsSlice';
 import { selectThisWeekSDongils } from '@store/slices/thisWeekSDongilsSlice';
-import { selectWalkingDongils } from '@store/slices/walkingDongilsSlice';
+import { useQuery } from 'react-query';
 
 function useTargetDongil(id: string) {
   const isKid = useAppSelector(selectIsKid);
   const selectedKid = useAppSelector(selectSelectedKid);
-  const walkingDongils = useAppSelector(selectWalkingDongils);
   const thisWeekSDongils = useAppSelector(selectThisWeekSDongils);
+
+  const { data: walkingDongils } = useQuery(
+    [queryKeys.CHALLENGE, 'walking'],
+    () => challengeAPI.getChallenge('walking'),
+  );
 
   if (isKid === true) {
     return walkingDongils?.find(
