8498201bad595a716922d02e56e7ecb95e6cd55e
한규진 <david0218@naver.com>
2022-09-20 17:16:53 +0900

    fix : 돈길 걷기 리액트 쿼리 마이그레이션

--- src/store/slices/walkingDongilsSlice.tsx
+++ src/store/slices/walkingDongilsSlice.tsx
@@ -13,53 +13,20 @@ const initialState: IWalkingDongilsState = {
   isWalkingDongilsPatched: false,
 };
 
-// PATCH: 돈길 걷기
-export const walkDongil = createAsyncThunk(
-  'walkingDongils/walk',
-  async (thunkPayload: { axiosPrivate: AxiosInstance; id: number }) => {
-    const { axiosPrivate, id } = thunkPayload;
-    const response = await axiosPrivate.patch(`/progress/${id}`);
-    return response.data;
-  },
-);
-
 export const walkingDongilsSlice = createSlice({
   name: 'walkingDongils',
   initialState,
   reducers: {
-    dispatchResetIsPatched(state) {
+    resetDongilPatched(state) {
       state.isWalkingDongilsPatched = false;
     },
-    dispatchSetPatched(state, action) {
-      const id = action.payload.id;
-      const achievedDongil = state.walkingDongils!.find(
-        (dongil) => dongil.id === id,
-      );
-      if (achievedDongil?.progressList) {
-        achievedDongil.progressList[
-          achievedDongil.progressList?.length - 1
-        ].isAchieved = true;
-      }
+    setDongilPatched(state) {
       state.isWalkingDongilsPatched = true;
     },
-  },
-  extraReducers(builder) {
-    builder.addCase(walkDongil.fulfilled, (state, action) => {
-      const { id } = action.meta.arg;
-      const achievedDongil = state.walkingDongils!.find(
-        (dongil) => dongil.id === id,
-      );
-      if (achievedDongil?.progressList) {
-        achievedDongil.progressList[
-          achievedDongil.progressList?.length - 1
-        ].isAchieved = true;
-      }
-      state.isWalkingDongilsPatched = true;
-    });
   },
 });
 
-export const { dispatchResetIsPatched, dispatchSetPatched } =
+export const { resetDongilPatched, setDongilPatched } =
   walkingDongilsSlice.actions;
 
 export const selectWalkingDongils = (state: RootState) =>
