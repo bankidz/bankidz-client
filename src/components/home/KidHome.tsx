dccfdf3662cef302b65903307dd620ed9bf360ae
24siefil <24siefil@gmail.com>
2022-10-07 00:51:18 +0900

    fix: 온보딩 과정 뒤로가기 버튼 클릭 시 로그아웃, 튜토리얼 UI 버그 해소

--- src/components/home/KidHome.tsx
+++ src/components/home/KidHome.tsx
@@ -7,12 +7,6 @@ import { useQuery } from 'react-query';
 import queryKeys from '@lib/constants/queryKeys';
 import challengeAPI from '@lib/apis/challenge/challengeAPI';
 
-/**
- * 홈 페이지 최초 진입 시 주간 진행상황, 걷고있는 돈길 리스트, 대기중인 돈길 리스트를 순차적으로 fetch 합니다.
- * 이후에 홈 페이지 재 진입 시는 해당 데이터를 fetch 하지 않습니다.
- * 홈 페이지에 랜더링 되는 각 UI는 API 단위로 분할된 JSX를 반환하는 함수로 관리됩니다.
- * 해당 함수에서 반환하는 JSX는 RTK slice 내부의 fetchStatus에 따라 적절한 값으로 변화합니다.
- */
 function KidHome() {
   const { status: kidSummaryStatus, data: kidSummary } = useQuery(
     queryKeys.CHALLENGE_PROGRESS,
