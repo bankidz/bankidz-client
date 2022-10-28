c3e7ced1b96af633a7afd41b69ef73575da712ff
24siefil <24siefil@gmail.com>
2022-10-28 14:48:45 +0900

    fix: WalkingDongilItem story 오류

--- src/components/home/walking/WalkingDongilItem.stories.tsx
+++ src/components/home/walking/WalkingDongilItem.stories.tsx
@@ -1,7 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import MarginTemplate from '@components/layout/MarginTemplate';
 import WalkingDongilItem from './WalkingDongilItem';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default {
   title: 'home/walking/WalkingDongilItem',
@@ -10,11 +9,7 @@ export default {
   decorators: [
     (Story) => (
       <MarginTemplate>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<Story />} />
-          </Routes>
-        </BrowserRouter>
+        <Story />
       </MarginTemplate>
     ),
   ],
@@ -24,18 +19,20 @@ const Template: ComponentStory<typeof WalkingDongilItem> = (args) => (
   <WalkingDongilItem {...args} />
 );
 
-export const 핸드폰_케이스_사기 = Template.bind({});
-핸드폰_케이스_사기.args = {
-  itemName: '전자제품',
-  title: '핸드폰 케이스 사기',
-  id: 1,
-  challengeStatus: 'WALKING',
-};
-
 export const 완구_퍼펙트걸_되기 = Template.bind({});
 완구_퍼펙트걸_되기.args = {
   itemName: '학용품',
   title: '완구 퍼펙트걸 되기',
   id: 2,
+  challengeStatus: 'WALKING',
+  interestRate: 20,
+};
+
+export const 프리페라 = Template.bind({});
+프리페라.args = {
+  itemName: '패션뷰티',
+  title: '프리페라 세트 사기',
+  id: 2,
   challengeStatus: 'FAILED',
+  interestRate: 30,
 };
