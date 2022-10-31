867f3fa6d131a45ad816d806f9f33fa452c511e9
한규진 <david0218@naver.com>
2022-10-31 11:33:53 +0900

    fix : lint error

--- src/components/common/buttons/Button.stories.tsx
+++ src/components/common/buttons/Button.stories.tsx
@@ -1,7 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
-import MarginTemplate from '@components/layout/MarginTemplate';
-
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'common/buttons/Button',
