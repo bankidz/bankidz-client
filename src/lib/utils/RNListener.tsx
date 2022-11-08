c61e75c1dbd4c654533b9f2a7bcf56cfa65ffb2f
24siefil <24siefil@gmail.com>
2022-11-08 18:12:01 +0900

    chore: listener alert

--- src/lib/utils/RNListener.tsx
+++ src/lib/utils/RNListener.tsx
@@ -6,24 +6,28 @@ function RNListener() {
   console.log('RNListener');
 
   const listener = async (event: any) => {
+    alert('listener');
+
+    const accessToken = getLocalStorage('accessToken');
+    alert(`accessToken: ${JSON.stringify(accessToken)}`);
     const oldEXPOToken = getLocalStorage('EXPOToken');
     alert(`oldEXPOToken: ${JSON.stringify(oldEXPOToken)}`);
 
-    // if (!oldEXPOToken) {
-    const newEXPOToken = event.data;
-    alert(`newEXPOToken: ${JSON.stringify(newEXPOToken)}`);
+    if (accessToken && !oldEXPOToken) {
+      const newEXPOToken = event.data;
+      alert(`newEXPOToken: ${JSON.stringify(newEXPOToken)}`);
 
-    try {
-      const response = await axiosPrivate.patch('/user/expo', {
-        expoToken: newEXPOToken,
-      });
-      alert(`response: ${JSON.stringify(response)}`);
+      try {
+        const response = await axiosPrivate.patch('/user/expo', {
+          expoToken: newEXPOToken,
+        });
+        alert(`response: ${JSON.stringify(response)}`);
 
-      setLocalStorage('EXPOToken', newEXPOToken);
-    } catch (error: any) {
-      alert(`error: ${JSON.stringify(error)}`);
+        setLocalStorage('EXPOToken', newEXPOToken);
+      } catch (error: any) {
+        alert(`error: ${JSON.stringify(error)}`);
+      }
     }
-    // }
   };
 
   if (window.ReactNativeWebView) {
